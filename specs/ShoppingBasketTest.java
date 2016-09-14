1f376718916f10561e98266500f303d07a59d5b5
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-09-14 16:07:39 +0100

    Setup both the shopping basket and its asociated test

---
+++ specs/ShoppingBasketTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import shop.*;
+
+public class ShoppingBasketTest{
+
+  Customer loyalCustomer;
+  Customer customer;
+  Item daz;
+  Item cheese;
+
+  ShoppingBasket basket;
+
+}
